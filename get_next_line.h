f97811733745d5762780cefcba4047c78c055b25
AnaKing228 <kronos5525@yandex.ru>
2020-06-17 14:05:53 +0300

    bronze III

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjani <cjani@studen.21-school.ru>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/17 14:05:14 by flexer            #+#    #+#             */
+/*   Updated: 2020/06/17 14:05:35 by cjani            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *prestr, char const *sufstr);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 20
+# endif
+#endif
